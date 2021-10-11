dbbde15164b0a9c551d05a986917237e49747a9c
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-10-11 09:35:52 -0300

    Create Aluno component

---
+++ projeto/src/components/Aluno.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import '../styles/aluno.css';
+
+export default class Aluno extends Component {
+  render() {
+    const { nome, idade, curso, cidade, email } = this.props;
+
+    return (
+      <div className="aluno">
+        <p>Nome: {nome}</p>
+        <p>Idade: {idade} anos</p>
+        <p>Curso: {curso}</p>
+        <p>Cidade Origem: {cidade}</p>
+        <p>Email: {email}</p>
+      </div>
+    );
+  }
+}
+
+Aluno.propTypes = {
+  nome: PropTypes.string.isRequired,
+  idade: PropTypes.number.isRequired,
+  curso: PropTypes.string.isRequired,
+  cidade: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
