9d476fe5f0363c6e48fc207e2a25581a354b8b0a
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-10-24 23:58:16 -0300

    Questao 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import World from './atividade_2/components/World';
 import Arena from './atividade_2/components/Arena';
 import Enemy from './atividade_2/components/Enemy';
 import Hero from './atividade_2/components/Hero';
@@ -9,10 +10,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Arena arena="Tokyo Dome - Underground Area">
-          <Hero name="Gato de Botas" img={heroUrl} />
-          <Enemy name="Ultra Duck" img={enemyUrl} />
-        </Arena>
+        <World>
+          <Arena arena="Castelão 1">
+            <Hero name="Gato de Botas" img={heroUrl} />
+            <Enemy name="Ultra Duck" img={enemyUrl} />
+          </Arena>
+          <Arena arena="Castelão 2">
+            <Hero name="Gato de Botas" img={heroUrl} />
+            <Enemy name="Ultra Duck" img={enemyUrl} />
+          </Arena>
+          <Arena arena="Castelão 3">
+            <Hero name="Gato de Botas" img={heroUrl} />
+            <Enemy name="Ultra Duck" img={enemyUrl} />
+          </Arena>
+        </World>
       </header>
     </div>
   );
