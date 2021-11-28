4db7c0aae7dc120c13d5edd8913da351ff012412
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-11-28 03:18:08 -0300

    Add BrowserRouter to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Atividade4 from './atividade_4/Atividade4';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Atividade4 />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Atividade4 />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
