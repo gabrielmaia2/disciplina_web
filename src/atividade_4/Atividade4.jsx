cbae35b9698f7b3de6bb496ee8697e2651b4aeb3
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-11-28 03:27:09 -0300

    Create Navbar on Atividade4

--- src/atividade_4/Atividade4.jsx
+++ src/atividade_4/Atividade4.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 import AddAluno from './hooks/AddAluno';
+import ListAlunos from './hooks/ListAlunos';
 
 export default function Atividade4() {
   return (
     <div>
-      <AddAluno />
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Link className="navbar-brand" to="/">Crud alunos</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Listar</Nav.Link>
+              <Nav.Link as={Link} to="/add">Adicionar</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/add" element={<AddAluno />} />
+        <Route path="/" element={<ListAlunos />} />
+      </Routes>
     </div>
   );
 }
