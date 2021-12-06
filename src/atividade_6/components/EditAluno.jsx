fe0f837a25f09dc3f64a0cae7dbb825ce24b6ef6
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-12-06 08:10:12 -0300

    chore(atividade06): Copy files from atividade_04 to atividade_06

---
+++ src/atividade_6/components/EditAluno.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export default function EditAluno() {
+  const [nome, setNome] = useState('');
+  const [curso, setCurso] = useState('');
+  const [IRA, setIRA] = useState(0);
+  const [error, setError] = useState(null);
+  const updateIRA = (e) => {
+    const invalid = Number.isNaN(+e.target.value);
+    if (!invalid) { setIRA(e.target.value); }
+  };
+  const navigate = useNavigate();
+  const params = useParams();
+  const { id } = params;
+
+  const addAluno = async (event) => {
+    event.preventDefault();
+    try {
+      const aluno = { nome, curso, IRA };
+      const { status, data: { success } } = await axios.post('/alunos/post', aluno);
+      if (status === 200 && success) {
+        setNome('');
+        setCurso('');
+        setIRA(0);
+      } else {
+        setError(Error('Falha ao adicionar aluno'));
+      }
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  const editAluno = async (event) => {
+    event.preventDefault();
+    try {
+      const aluno = { nome, curso, IRA };
+      const { status, data: { success } } = await axios.put(`/alunos/put/${id}`, aluno);
+      if (status === 200 && success) {
+        navigate('/');
+      } else {
+        setError(Error('Falha ao atualizar aluno'));
+      }
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  const handleSubmit = id === undefined ? addAluno : editAluno;
+
+  useEffect(() => {
+    if (id) {
+      const fetchData = async () => {
+        try {
+          const { status, data: { success, aluno } } = await axios.get(`/alunos/get/${id}`);
+          if (status === 200 && success) {
+            setNome(aluno.nome);
+            setCurso(aluno.curso);
+            setIRA(aluno.IRA);
+          } else {
+            setError(Error('Falha ao buscar dados do aluno'));
+          }
+        } catch (e) {
+          setError(e);
+        }
+      };
+      fetchData();
+    }
+  }, [id]);
+
+  if (error) {
+    return (
+      <Alert variant="danger">
+        Um erro ocorreu
+        {String(error)}
+      </Alert>
+    );
+  }
+
+  return (
+    <div>
+      <h1 className="text-center p-2">{id === undefined ? 'Adicionar aluno' : 'Editar aluno' }</h1>
+      <Form onSubmit={handleSubmit} className="w-25 mx-auto">
+        <Form.Group className="mb-3" controlId="formNome">
+          <Form.Label className="mx-1">Nome</Form.Label>
+          <Form.Control type="text" placeholder="Insira nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formCurso">
+          <Form.Label className="mx-1">Curso</Form.Label>
+          <Form.Control type="text" placeholder="Insira curso" value={curso} onChange={(e) => setCurso(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formIRA">
+          <Form.Label className="mx-1">IRA</Form.Label>
+          <Form.Control type="text" placeholder="Insira IRA" value={IRA} onChange={updateIRA} />
+        </Form.Group>
+
+        <Button className="mx-1" variant="primary" type="submit">
+          {id === undefined ? 'Adicionar' : 'Editar' }
+        </Button>
+      </Form>
+    </div>
+  );
+}
---
+++ src/atividade_6/components/ListAlunos.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {
+  Col, Row, Button, Container, Alert,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function ListAlunos() {
+  const [alunos, setAlunos] = useState([]);
+  const [error, setError] = useState(null);
+  const [updater, setUpdater] = useState(false);
+  const updateList = () => setUpdater(!updater);
+
+  useEffect(() => {
+    const fetchAlunos = async () => {
+      try {
+        const { status, data: { success, alunos: newAlunos } } = await axios.get('/alunos/getall');
+        if (status === 200 && success) {
+          setAlunos(newAlunos);
+        } else {
+          setError(Error('Falha ao buscar lista de alunos'));
+        }
+      } catch (e) {
+        setError(e);
+      }
+    };
+    fetchAlunos();
+  }, [updater]);
+
+  const deleteAluno = async (id) => {
+    try {
+      const { status, data: { success } } = await axios.delete(`/alunos/delete/${id}`);
+      if (status === 200 && success) {
+        updateList();
+      } else {
+        setError(Error(`Falha ao deletar aluno com id id ${id}`));
+      }
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  if (error) {
+    return (
+      <Alert variant="danger">
+        {String(error)}
+      </Alert>
+    );
+  }
+
+  let alunosView = [];
+  alunos.forEach((a) => alunosView.push(
+    <Row key={a.id}>
+      <Col xs={0} className="border">{a.id}</Col>
+      <Col xs={0} className="border">{a.nome}</Col>
+      <Col xs={0} className="border">{a.curso}</Col>
+      <Col xs={0} className="border">{a.IRA}</Col>
+      <Col xs="auto"><Button as={Link} to={`/edit/${a.id}`} variant="primary" className="m-1">Editar</Button></Col>
+      <Col xs="auto"><Button onClick={() => deleteAluno(a.id)} variant="danger" className="m-1">Deletar</Button></Col>
+    </Row>,
+  ));
+
+  if (alunos.length === 0) { alunosView = <Row><Col className="border">Lista vazia</Col></Row>; }
+
+  return (
+    <div className="text-center">
+      <h1 className="p-2">Alunos</h1>
+      <Container fluid>
+        {alunosView}
+      </Container>
+      <Button className="p-2 m-2" variant="primary" onClick={updateList}>Atualizar lista</Button>
+    </div>
+  );
+}
